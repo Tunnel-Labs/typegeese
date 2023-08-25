9c6c5a9ae8ecdfdf31d9139f8cecf4d6fe4e52a5
Leon <leon@leondreamed.com>
2023-08-25 19:56:01 -0400

    fix

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,6 +2,7 @@ import { createPackageBuilder } from "lionconfig";
 
 await createPackageBuilder(import.meta, {
   packageJsonPath: "../package.json",
+  tsconfigPath: "../tsconfig.build.json",
 })
   .cleanDistFolder()
   .tsc()
