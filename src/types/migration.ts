f0a79f5bd678359242c37276c8964d9c4700b488
Leon <leon@leondreamed.com>
2023-08-27 15:57:02 -0400

    fix: migrations type

--- src/types/migration.ts
+++ src/types/migration.ts
@@ -3,16 +3,25 @@ import type { Promisable } from "type-fest";
 import type { Deprecated } from "~/types/deprecated.js";
 import type { NormalizedHyperschema } from "~/types/hyperschema.js";
 import type { ModelSchema } from "~/classes/index.js";
+import type { VirtualForeignRef } from "~/types/refs.js";
 
 export type Diff<T, V> = {
   [P in Exclude<keyof T, keyof V>]: T[P];
+};
+
+export type ExcludeVirtualForeignRefs<Model> = {
+  [K in keyof Model as Model[K] extends VirtualForeignRef<any, any, any>
+    ? never
+    : Model[K] extends VirtualForeignRef<any, any, any>[]
+    ? never
+    : K]: Model[K];
 };
 
 // prettier-ignore
 export type IsSupersetKey<
 	PreviousModel,
 	CurrentModel,
-	Key extends keyof CurrentModel
+	Key extends keyof ExcludeVirtualForeignRefs<CurrentModel>
 > =
 	Key extends '_version'
 		? true
@@ -24,10 +33,10 @@ export type IsSupersetKey<
 				: CurrentModel[Key] extends PreviousModel[Key]
 				? true
 				: false
-			: true;
+		: true;
 
 export type NonSupersetKeys<PreviousModel, CurrentModel> = keyof {
-  [K in keyof CurrentModel as IsSupersetKey<
+  [K in keyof ExcludeVirtualForeignRefs<CurrentModel> as IsSupersetKey<
     PreviousModel,
     CurrentModel,
     K
@@ -47,7 +56,10 @@ export type MigrationFunctions<PreviousModel, CurrentModel> =
 			? null
 		: NonSupersetKeys<PreviousModel, CurrentModel> extends never
 		? {
-				[K in keyof Diff<CurrentModel, PreviousModel>]: (
+				[K in keyof Diff<
+					ExcludeVirtualForeignRefs<CurrentModel>,
+					ExcludeVirtualForeignRefs<PreviousModel>
+				>]: (
 					this: DocumentType<CurrentModel>
 				) => Promisable<CurrentModel[K]>;
 			}
