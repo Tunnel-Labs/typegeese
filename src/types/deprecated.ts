83c861ad4ab42badc39e20756148fa3e37f7e69b
Leon Si <leon@leondreamed.com>
2023-08-30 18:56:48 -0400

    0.0.60

--- src/types/deprecated.ts
+++ src/types/deprecated.ts
@@ -1,4 +1,9 @@
 import type { IsNever } from 'type-fest';
 
 export type Deprecated<T> = never;
-export type IsDeprecated<Value> = IsNever<Value>;
+export type IsNeverArray<T> = [T] extends [never[]] ? true : false;
+// prettier-ignore
+export type IsDeprecated<Value> =
+	| IsNever<Value> extends true ? true
+	: IsNeverArray<Value> extends true ? true
+	: false;
