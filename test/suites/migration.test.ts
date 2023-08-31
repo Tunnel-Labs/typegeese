5b488e8a50afbc0616f32e5262a80960a609bc30
Leon Si <leon@leondreamed.com>
2023-08-30 21:15:51 -0400

    chore: clean imports

--- test/suites/migration.test.ts
+++ test/suites/migration.test.ts
@@ -1,6 +1,5 @@
 import { beforeAll, expect, test } from 'vitest';
 import { CreateInput, getModelForHyperschema, select } from '~/index.js';
-import type { Post, Comment } from '~test/fixtures/blog/models/$schemas.js';
 import { createId } from '@paralleldrive/cuid2';
 import { getModels } from '~test/fixtures/blog/models/$models.js';
 import { getMongoose } from '~test/utils/mongoose.js';
